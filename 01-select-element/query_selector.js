d56ce6d69e638de69663066adb419e287babd2b9
Matiyas8 <matnig7@gmail.com>
2021-06-28 16:22:46 +0200

    exercise #1 done

--- 01-select-element/query_selector.js
+++ 01-select-element/query_selector.js
@@ -1,1 +1,7 @@
 // write your code here
+const citiesList = document.querySelector("#fifth");
+
+console.log(citiesList.innerHTML);
+
+const selectedcity = document.querySelector("#selected");
+selectedcity.innerHTML = citiesList.innerHTML
